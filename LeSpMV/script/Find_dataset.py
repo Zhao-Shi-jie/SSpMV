ba328841481da5e1742c82e824e2320768145fc1
lsl036 <497851540@qq.com>
2024-03-22 14:09:08 +0800

    Run CSR with 2833 suitesparse data

---
+++ LeSpMV/script/Find_dataset.py
@@ -0,0 +1,28 @@
+import subprocess
+import os
+import pandas as pd
+
+def read_full_dataset(excel_path):
+    df = pd.read_excel(excel_path)
+    full_dataset = set(df[['Id', 'Name']].itertuples(index=False, name=None))
+    return full_dataset
+
+if __name__ == "__main__":
+    All_Suite_data = read_full_dataset("./NNZ low to High.xlsx")
+    
+    # 读取已测试数据集的Excel文件
+    tested_df = pd.read_excel('CSR Res.xlsx')
+    
+    # 去除重复项，只保留唯一的ID和Name组合
+    unique_tested_dataset = tested_df[['Id', 'Name']].drop_duplicates()
+    
+    # 将去除重复数据后的已测试数据集转换为集合
+    unique_tested_set = set(unique_tested_dataset.itertuples(index=False, name=None))
+    
+    # 获取未测试的数据集
+    untested_datasets = All_Suite_data - unique_tested_set
+    
+    # 打印结果
+    print("Untested datasets:")
+    for untested in untested_datasets:
+        print(untested)
--- LeSpMV/script/run_spmv.py
+++ LeSpMV/script/run_spmv.py
@@ -43,6 +43,35 @@ def Run_CSR_intKernel(excel_path):
             except subprocess.CalledProcessError as e:
                 print(f"An error occurred while executing {command}")
                 print(e)
+
+def Run_BSR_intKernel(excel_path):
+    
+    # 读dataset list 并生成数据组：
+    mtx_info = Read_TestDataset(excel_path)
+    
+    # 定义测试命令中不变的参数
+    index = 0
+    precision = 64
+    sche = range(0,4)
+    
+    for matID, name, mtx_path in mtx_info:
+        for sche_mod in sche:
+            # 构建并运行命令
+            command = f"./benchmark_spmv_bsr {mtx_path} --matID={matID} --Index={index} --precision={precision} --sche={sche_mod}"
+            print(f"Executing: {command}")
+            
+            # 执行命令
+            try:
+                result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                # 打印命令执行结果
+                print(f"Command finished with return code {result.returncode}")
+                if result.stdout:
+                    print(f"Output: {result.stdout.decode('utf-8')}")
+                if result.stderr:
+                    print(f"Errors: {result.stderr.decode('utf-8')}")
+            except subprocess.CalledProcessError as e:
+                print(f"An error occurred while executing {command}")
+                print(e)
             
 if __name__ == "__main__":
-    Run_CSR_intKernel("./NNZ low to High.xlsx")
+    Run_CSR_intKernel("./SuiteSparse_Matrix.xlsx")
