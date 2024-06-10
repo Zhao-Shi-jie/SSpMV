b0a7a1ece898db875eb3820b3c4302e532cafef4
lsl036 <497851540@qq.com>
2024-06-10 11:32:08 +0800

    test routine for BSR

--- LeSpMV/test/benchmark_spmv_bsr.cpp
+++ LeSpMV/test/benchmark_spmv_bsr.cpp
@@ -97,7 +97,7 @@ void run_bsr_kernels(int argc, char **argv)
     // 0: 串行， 1：omp并行， 2：omp load balanced
     // Our : {St,(==)StCont, Dyn, guided} x {omp, lb}
     for (int sche_mode = 0 ; sche_mode < 4; ++sche_mode){
-    for(int methods = 1; methods < 3; ++methods){
+    for(int methods = 2; methods < 3; ++methods){
         msec_per_iteration = test_bsr_matrix_kernels(csr, methods, sche_mode);
         fflush(stdout);
 
